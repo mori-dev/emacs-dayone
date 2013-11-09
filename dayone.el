0955b2e12e1c5860016dc2de1e9b868e1a4f8ffd
Syohei YOSHIDA <syohex@gmail.com>
2013-11-09 10:02:16 +0900

    Use defvar instead of setq for byte-compile warnings

--- dayone.el
+++ dayone.el
@@ -71,7 +71,7 @@
     (dayone-set-xml uuid)
     (dayone-write-file uuid)))
 
-(setq dayone-xml "<?xml version='1.0' encoding='UTF-8'?>
+(defvar dayone-xml "<?xml version='1.0' encoding='UTF-8'?>
 <!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
 <plist version='1.0'>
 <dict>
